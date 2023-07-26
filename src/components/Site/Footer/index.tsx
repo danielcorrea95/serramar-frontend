149e30091b51a4ad1f9eb9666ff85dc3eea5e73a
danielcorrea95 <daniel-correa05@hotmail.com>
2023-07-26 16:18:57 -0300

    scrool ads

--- src/components/Site/Footer/index.tsx
+++ src/components/Site/Footer/index.tsx
@@ -24,12 +24,14 @@ export default function Footer() {
       loop: true,
     },
     [
-      (slider) => {
-        let timeout
+      (slider: any) => {
+        let timeout: any
         let mouseOver = false
+
         function clearNextTimeout() {
           clearTimeout(timeout)
         }
+
         function nextTimeout() {
           clearTimeout(timeout)
           if (mouseOver) return
@@ -37,24 +39,27 @@ export default function Footer() {
             slider.next()
           }, 2000)
         }
+
         slider.on('created', () => {
           slider.container.addEventListener('mouseover', () => {
             mouseOver = true
             clearNextTimeout()
           })
+
           slider.container.addEventListener('mouseout', () => {
             mouseOver = false
             nextTimeout()
           })
+
           nextTimeout()
         })
+
         slider.on('dragStarted', clearNextTimeout)
         slider.on('animationEnded', nextTimeout)
         slider.on('updated', nextTimeout)
       },
     ],
   )
-
   return (
     <Container>
       <Align>
