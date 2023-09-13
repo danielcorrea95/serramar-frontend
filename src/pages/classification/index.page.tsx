cf78abf81a0a9351067d3ac6edb7a16752e51c88
danielcorrea95 <daniel-correa05@hotmail.com>
2023-09-13 09:15:09 -0300

    update classification

--- src/pages/classification/index.page.tsx
+++ src/pages/classification/index.page.tsx
@@ -21,11 +21,12 @@ export default function Classification() {
                   <tr>
                     <td></td>
                     <td>Serramar</td>
-                    <td>P</td>
                     <td>J</td>
+                    <td>P</td>
                     <td>V</td>
                     <td>E</td>
                     <td>D</td>
+                    <td>SG</td>
                     <td>GP</td>
                     <td>GC</td>
                     <td>CA</td>
@@ -35,145 +36,142 @@ export default function Classification() {
                 <tbody>
                   <tr>
                     <td>1º</td> {/* POSICAO  */}
-                    <td>Mar Azul</td> {/* EQUIPE  */}
-                    <td>1</td> {/* PONTOS  */}
+                    <td>Central</td> {/* EQUIPE  */}
                     <td>1</td> {/* JOGOS  */}
-                    <td>0</td> {/* VITORIAS  */}
-                    <td>1</td> {/* EMPATES  */}
+                    <td>3</td> {/* PONTOS  */}
+                    <td>1</td> {/* VITORIAS  */}
+                    <td>0</td> {/* EMPATES  */}
                     <td>0</td> {/* DERROTAS  */}
-                    <td>1</td> {/* GOLS PRO  */}
+                    <td>+2</td> {/* SALDO DE GOLS  */}
+                    <td>3</td> {/* GOLS PRO  */}
                     <td>1</td> {/* GOLS CONTRA  */}
-                    <td>2</td> {/* CARTAO AMARELO  */}
+                    <td>4</td> {/* CARTAO AMARELO  */}
                     <td>0</td> {/* CARTAO VERMELHO  */}
                   </tr>
                   <tr>
                     <td>2º</td> {/* POSICAO  */}
-                    <td>Serraria</td> {/* EQUIPE  */}
-                    <td>1</td> {/* PONTOS  */}
+                    <td>Xangri-lá</td> {/* EQUIPE  */}
                     <td>1</td> {/* JOGOS  */}
+                    <td>3</td> {/* PONTOS  */}
+                    <td>1</td> {/* VITORIAS  */}
+                    <td>0</td> {/* EMPATES  */}
+                    <td>0</td> {/* DERROTAS  */}
+                    <td>+1</td> {/* SALDO DE GOLS  */}
+                    <td>2</td> {/* GOLS PRO  */}
+                    <td>1</td> {/* GOLS CONTRA  */}
+                    <td>4</td> {/* CARTAO AMARELO  */}
+                    <td>0</td> {/* CARTAO VERMELHO  */}
+                  </tr>
+                  <tr>
+                    <td>3º</td> {/* POSICAO  */}
+                    <td>Farroupilha</td> {/* EQUIPE  */}
+                    <td>1</td> {/* JOGOS  */}
+                    <td>3</td> {/* PONTOS  */}
+                    <td>1</td> {/* VITORIAS  */}
+                    <td>0</td> {/* EMPATES  */}
+                    <td>0</td> {/* DERROTAS  */}
+                    <td>+1</td> {/* SALDO DE GOLS  */}
+                    <td>1</td> {/* GOLS PRO  */}
+                    <td>0</td> {/* GOLS CONTRA  */}
+                    <td>3</td> {/* CARTAO AMARELO  */}
+                    <td>2</td> {/* CARTAO VERMELHO  */}
+                  </tr>
+                  <tr>
+                    <td>4º</td> {/* POSICAO  */}
+                    <td>Aliança</td> {/* EQUIPE  */}
+                    <td>1</td> {/* JOGOS  */}
+                    <td>1</td> {/* PONTOS  */}
                     <td>0</td> {/* VITORIAS  */}
                     <td>1</td> {/* EMPATES  */}
                     <td>0</td> {/* DERROTAS  */}
+                    <td>0</td> {/* SALDO DE GOLS  */}
                     <td>1</td> {/* GOLS PRO  */}
                     <td>1</td> {/* GOLS CONTRA  */}
                     <td>2</td> {/* CARTAO AMARELO  */}
                     <td>0</td> {/* CARTAO VERMELHO  */}
                   </tr>
                   <tr>
-                    <td>3º</td> {/* POSICAO  */}
-                    <td>Aliança</td> {/* EQUIPE  */}
-                    <td>0</td> {/* PONTOS  */}
-                    <td>0</td> {/* JOGOS  */}
-                    <td>0</td> {/* VITORIAS  */}
-                    <td>0</td> {/* EMPATES  */}
-                    <td>0</td> {/* DERROTAS  */}
-                    <td>0</td> {/* GOLS PRO  */}
-                    <td>0</td> {/* GOLS CONTRA  */}
-                    <td>0</td> {/* CARTAO AMARELO  */}
-                    <td>0</td> {/* CARTAO VERMELHO  */}
-                  </tr>
-                  <tr>
-                    <td>4º</td> {/* POSICAO  */}
-                    <td>Central</td> {/* EQUIPE  */}
-                    <td>0</td> {/* PONTOS  */}
-                    <td>0</td> {/* JOGOS  */}
-                    <td>0</td> {/* VITORIAS  */}
-                    <td>0</td> {/* EMPATES  */}
-                    <td>0</td> {/* DERROTAS  */}
-                    <td>0</td> {/* GOLS PRO  */}
-                    <td>0</td> {/* GOLS CONTRA  */}
-                    <td>0</td> {/* CARTAO AMARELO  */}
-                    <td>0</td> {/* CARTAO VERMELHO  */}
-                  </tr>
-                  <tr>
                     <td>5º</td> {/* POSICAO  */}
-                    <td>Farroupilha</td> {/* EQUIPE  */}
-                    <td>0</td> {/* PONTOS  */}
-                    <td>0</td> {/* JOGOS  */}
+                    <td>GAO</td> {/* EQUIPE  */}
+                    <td>1</td> {/* JOGOS  */}
+                    <td>1</td> {/* PONTOS  */}
                     <td>0</td> {/* VITORIAS  */}
-                    <td>0</td> {/* EMPATES  */}
+                    <td>1</td> {/* EMPATES  */}
                     <td>0</td> {/* DERROTAS  */}
-                    <td>0</td> {/* GOLS PRO  */}
-                    <td>0</td> {/* GOLS CONTRA  */}
-                    <td>0</td> {/* CARTAO AMARELO  */}
+                    <td>0</td> {/* SALDO DE GOLS  */}
+                    <td>1</td> {/* GOLS PRO  */}
+                    <td>1</td> {/* GOLS CONTRA  */}
+                    <td>2</td> {/* CARTAO AMARELO  */}
                     <td>0</td> {/* CARTAO VERMELHO  */}
                   </tr>
                   <tr>
                     <td>6º</td> {/* POSICAO  */}
-                    <td>GAO</td> {/* EQUIPE  */}
-                    <td>0</td> {/* PONTOS  */}
-                    <td>0</td> {/* JOGOS  */}
+                    <td>Mar Azul</td> {/* EQUIPE  */}
+                    <td>1</td> {/* JOGOS  */}
+                    <td>1</td> {/* PONTOS  */}
                     <td>0</td> {/* VITORIAS  */}
-                    <td>0</td> {/* EMPATES  */}
+                    <td>1</td> {/* EMPATES  */}
                     <td>0</td> {/* DERROTAS  */}
-                    <td>0</td> {/* GOLS PRO  */}
-                    <td>0</td> {/* GOLS CONTRA  */}
-                    <td>0</td> {/* CARTAO AMARELO  */}
+                    <td>0</td> {/* SALDO DE GOLS  */}
+                    <td>1</td> {/* GOLS PRO  */}
+                    <td>1</td> {/* GOLS CONTRA  */}
+                    <td>2</td> {/* CARTAO AMARELO  */}
                     <td>0</td> {/* CARTAO VERMELHO  */}
                   </tr>
                   <tr>
                     <td>7º</td> {/* POSICAO  */}
-                    <td>Minuano</td> {/* EQUIPE  */}
-                    <td>0</td> {/* PONTOS  */}
-                    <td>0</td> {/* JOGOS  */}
+                    <td>Serraria</td> {/* EQUIPE  */}
+                    <td>1</td> {/* JOGOS  */}
+                    <td>1</td> {/* PONTOS  */}
                     <td>0</td> {/* VITORIAS  */}
-                    <td>0</td> {/* EMPATES  */}
+                    <td>1</td> {/* EMPATES  */}
                     <td>0</td> {/* DERROTAS  */}
-                    <td>0</td> {/* GOLS PRO  */}
-                    <td>0</td> {/* GOLS CONTRA  */}
-                    <td>0</td> {/* CARTAO AMARELO  */}
+                    <td>0</td> {/* SALDO DE GOLS  */}
+                    <td>1</td> {/* GOLS PRO  */}
+                    <td>1</td> {/* GOLS CONTRA  */}
+                    <td>2</td> {/* CARTAO AMARELO  */}
                     <td>0</td> {/* CARTAO VERMELHO  */}
                   </tr>
                   <tr>
                     <td>8º</td> {/* POSICAO  */}
-                    <td>Serraria</td> {/* EQUIPE  */}
+                    <td>União</td> {/* EQUIPE  */}
+                    <td>1</td> {/* JOGOS  */}
                     <td>0</td> {/* PONTOS  */}
-                    <td>0</td> {/* JOGOS  */}
                     <td>0</td> {/* VITORIAS  */}
                     <td>0</td> {/* EMPATES  */}
-                    <td>0</td> {/* DERROTAS  */}
-                    <td>0</td> {/* GOLS PRO  */}
-                    <td>0</td> {/* GOLS CONTRA  */}
-                    <td>0</td> {/* CARTAO AMARELO  */}
+                    <td>1</td> {/* DERROTAS  */}
+                    <td>-1</td> {/* SALDO DE GOLS  */}
+                    <td>1</td> {/* GOLS PRO  */}
+                    <td>2</td> {/* GOLS CONTRA  */}
+                    <td>3</td> {/* CARTAO AMARELO  */}
                     <td>0</td> {/* CARTAO VERMELHO  */}
                   </tr>
                   <tr>
                     <td>9º</td> {/* POSICAO  */}
                     <td>Tramandaí</td> {/* EQUIPE  */}
+                    <td>1</td> {/* JOGOS  */}
                     <td>0</td> {/* PONTOS  */}
-                    <td>0</td> {/* JOGOS  */}
                     <td>0</td> {/* VITORIAS  */}
                     <td>0</td> {/* EMPATES  */}
-                    <td>0</td> {/* DERROTAS  */}
+                    <td>1</td> {/* DERROTAS  */}
+                    <td>-1</td> {/* SALDO DE GOLS  */}
                     <td>0</td> {/* GOLS PRO  */}
-                    <td>0</td> {/* GOLS CONTRA  */}
-                    <td>0</td> {/* CARTAO AMARELO  */}
+                    <td>1</td> {/* GOLS CONTRA  */}
+                    <td>5</td> {/* CARTAO AMARELO  */}
                     <td>0</td> {/* CARTAO VERMELHO  */}
                   </tr>
                   <tr>
                     <td>10º</td> {/* POSICAO  */}
-                    <td>União</td> {/* EQUIPE  */}
+                    <td>Minuano</td> {/* EQUIPE  */}
+                    <td>1</td> {/* JOGOS  */}
                     <td>0</td> {/* PONTOS  */}
-                    <td>0</td> {/* JOGOS  */}
                     <td>0</td> {/* VITORIAS  */}
                     <td>0</td> {/* EMPATES  */}
-                    <td>0</td> {/* DERROTAS  */}
-                    <td>0</td> {/* GOLS PRO  */}
-                    <td>0</td> {/* GOLS CONTRA  */}
-                    <td>0</td> {/* CARTAO AMARELO  */}
-                    <td>0</td> {/* CARTAO VERMELHO  */}
-                  </tr>
-                  <tr>
-                    <td>11º</td> {/* POSICAO  */}
-                    <td>Xangri-lá</td> {/* EQUIPE  */}
-                    <td>0</td> {/* PONTOS  */}
-                    <td>0</td> {/* JOGOS  */}
-                    <td>0</td> {/* VITORIAS  */}
-                    <td>0</td> {/* EMPATES  */}
-                    <td>0</td> {/* DERROTAS  */}
-                    <td>0</td> {/* GOLS PRO  */}
-                    <td>0</td> {/* GOLS CONTRA  */}
-                    <td>0</td> {/* CARTAO AMARELO  */}
+                    <td>1</td> {/* DERROTAS  */}
+                    <td>-2</td> {/* SALDO DE GOLS  */}
+                    <td>1</td> {/* GOLS PRO  */}
+                    <td>3</td> {/* GOLS CONTRA  */}
+                    <td>4</td> {/* CARTAO AMARELO  */}
                     <td>0</td> {/* CARTAO VERMELHO  */}
                   </tr>
                 </tbody>
